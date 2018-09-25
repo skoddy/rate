9c404f1197b5b2fb040e6da87e6effa60e18b514
skodowski <m.t.skodowski@gmail.com>
2018-09-25 17:27:14 +0200

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,7 @@ export class AppComponent {
       return [
         { title: 'Card 1', cols: 1, rows: 1 },
         { title: 'Card 2', cols: 1, rows: 1 },
-        
+
         { title: 'Card 3', cols: 1, rows: 1 },
         { title: 'Card 4', cols: 1, rows: 1 }
       ];
